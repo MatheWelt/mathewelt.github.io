94908c12e84683a37fd1d042a400439b31ca37f7
MrNerdastic <146835441+MrNerdastic@users.noreply.github.com>
2023-11-07 08:20:22 +0100

    Update index.js
    Added feature to show and save arithmetic operator

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 let num_1 = 1;
 let num_2 = 1;
+let sign = 1;
+let sign_string = "";
 document.getElementById("num_1").innerHTML = num_1.toString();
 document.getElementById("num_2").innerHTML = num_2.toString();
+if (sign == 1) {
+  sign_string = " + "
+}
+if (sign == 2) {
+  sign_string = " - "
+}
+if (sign == 3) {
+  sign_string = " / "
+}
+if (sign == 4) {
+  sign_string = " * "
+}
+document.getElementById("sign").innerHTML = sign_string;
