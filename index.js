c93215ecfc3981533f4461d0a76eb61a62b237b2
MrNerdastic <146835441+MrNerdastic@users.noreply.github.com>
2023-11-07 08:22:36 +0100

    Update index.js
    Changed how the variables are defined

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let num_1 = 1;
-let num_2 = 1;
-let sign = 1;
-let sign_string = "";
+var num_1 = 1;
+var num_2 = 1;
+var sign = 1;
+var sign_string = "";
 document.getElementById("num_1").innerHTML = num_1.toString();
 document.getElementById("num_2").innerHTML = num_2.toString();
 if (sign == 1) {
