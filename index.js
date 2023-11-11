29463e91cac850642921153e7de62093e4ec645f
MrNerdastic <146835441+MrNerdastic@users.noreply.github.com>
2023-11-11 12:55:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var num_1 = 1;
-var num_2 = 1;
-var sign = 1;
-var sign_string = "";
+let num_1 = 1;
+let num_2 = 1;
+let sign = 1;
+let sign_string = "";
 getElementById("num_1").innerHTML = num_1.toString();
 getElementById("num_2").innerHTML = num_2.toString();
 if (sign == 1) {
-  sign_string = " + "
+  sign_string = " + ";
 }
 if (sign == 2) {
-  sign_string = " - "
+  sign_string = " - ";
 }
 if (sign == 3) {
-  sign_string = " / "
+  sign_string = " / ";
 }
 if (sign == 4) {
-  sign_string = " * "
+  sign_string = " * ";
 }
 getElementById("sign").innerHTML = sign_string;
